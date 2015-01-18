0db7b1cb0af6336d14d49e91034cd9c0ec5e2f79
Clement MONDION <clement.mondion@gmail.com>
2015-01-18 23:43:42 +0100

    [clean]
    Cleaning repository

--- Controler.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'clementmondion'
-from model import *
-
-if __name__ == "__main__":
-    new = Player(raw_input("Name of the new player : "))
-    new.addExpense(raw_input("How much did he pay ? "))
